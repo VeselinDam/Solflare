dd40208f02762fcf9ad1892b2a8d31a057e0c8ae
VeselinDamnjanovicMegger <veselin.damnjanovic@megger.com>
2025-10-13 10:24:24 +0200

    add assert messages in test

--- test/specs/wallet-creation-and-recovery.e2e.ts
+++ test/specs/wallet-creation-and-recovery.e2e.ts
@@ -7,19 +7,19 @@ describe('Solflare Wallet Creation', () => {
         await HomePage.open();
 
         const pageTitleText = await HomePage.getPageTitleText();
-        await expect(pageTitleText).toEqual('YOUR WALLET. YOUR KINGDOM.');
+        await expect(pageTitleText).toHaveText('YOUR WALLET. YOUR KINGDOM.', { message: 'Home page title does not match expected text' });
         const createWallet = await HomePage.clickOnNeedNewWalletButton();
 
         // Verify that we are on the Create Wallet page
         const mainPageTitleText = await createWallet.getMainPageTitleText();
-        expect(mainPageTitleText).toEqual('Keys to Your Kingdom');
+        expect(mainPageTitleText).toHaveText('Keys to Your Kingdom', { message: 'Create Wallet page title does not match expected text' });
         const recoveryPhrases = await createWallet.getAllRecoveryPhrases();
         await createWallet.clickOnSaveMyRecoveryPhraseButton();
 
         // Confirm that we are on the recovery phrase confirmation page
         const recoveryPhraseTitleText = await createWallet.getRecoveryPhraseTitleText();
         expect(createWallet.recoveryPhraseTitle).toBeDisabled();
-        expect(recoveryPhraseTitleText).toEqual('Confirm Your Recovery Phrase');
+        expect(recoveryPhraseTitleText).toHaveText('Confirm Your Recovery Phrase', { message: 'Recovery Phrase title does not match expected text' });
 
         // Fill in the recovery phrase input fields in the correct order
         await createWallet.fillRecoveryPhraseInputFields(recoveryPhrases);
@@ -28,7 +28,7 @@ describe('Solflare Wallet Creation', () => {
         //Verify that we are on the Set Password page
         const passwordPageTitleText = await createWallet.getPasswordPageTitleText();
         expect(createWallet.passwordPageTitle).toBeDisabled();
-        expect(passwordPageTitleText).toEqual('Set a Password for Your Wallet');
+        expect(passwordPageTitleText).toHaveText('Set a Password for Your Wallet', { message: 'Password page title does not match expected text' });
         await createWallet.fillPasswordField('Test1234');
         await createWallet.fillRepeatPasswordInputFieldField('Test1234');
         await createWallet.clickOnContinueButton();
@@ -36,22 +36,22 @@ describe('Solflare Wallet Creation', () => {
         //Verify that we are on the Success page
         const successPageTitleText = await successPage.getPageTitleText();
         expect(successPage.pageTitle).toBeDisabled();
-        expect(successPageTitleText).toEqual('You’re All Set!');
+        expect(successPageTitleText).toHaveText('You’re All Set!', { message: 'Success page title does not match expected text' });
         const portfolioPage = await successPage.clickOnAgreeLetsGoButton();
 
         // Verify that we are on the Portfolio page
         const portfolioPageTitleText = await portfolioPage.getPageTitleText();
         expect(portfolioPage.pageTitle).toBeDisabled();
-        expect(portfolioPageTitleText).toEqual('Portfolio');
+        expect(portfolioPageTitleText).toHaveText('Portfolio', { message: 'Portfolio page title does not match expected text' });
         const walletManagementPage = await portfolioPage.clickOnwalletManagementAvatar();
 
         // Verify that we are on the Wallet Management page
         const walletManagementPageTitleText = await walletManagementPage.getElementText(walletManagementPage.pageTitle);
         expect(walletManagementPage.pageTitle).toBeDisabled();
-        expect(walletManagementPageTitleText).toEqual('My Wallets');
+        expect(walletManagementPageTitleText).toHaveText('My Wallets', { message: 'Wallet Management page title does not match expected text' });
         const mainWalletText = await walletManagementPage.getElementText(walletManagementPage.mainWallet);
         expect(walletManagementPage.mainWallet).toBeDisabled();
-        expect(mainWalletText).toEqual('Main Wallet');
+        expect(mainWalletText).toHaveText('Main Wallet', { message: 'Main Wallet title does not match expected text' });
 
         // Add a new wallet using the "Add" icon button
         await walletManagementPage.clickOnAddIconButton();
@@ -78,7 +78,7 @@ describe('Solflare Wallet Creation', () => {
 
         // Verify that we are on the Wallet Management page
         expect(walletManagementPage.pageTitle).toBeDisabled();
-        expect(walletManagementPageTitleText).toEqual('My Wallets');
+        expect(walletManagementPageTitleText).toHaveText('My Wallets', { message: 'Wallet Management page title does not match expected text' });
 
         // Verify that the subtitles of "My Wallets" match the selected wallets' titles
         const myWalletsSubtitles = await walletManagementPage.getMyWalletsSubtitles();
