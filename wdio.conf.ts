bc2dfcf35130c7fe229d85d23bda725a87635845
VeselinDamnjanovicMegger <veselin.damnjanovic@megger.com>
2025-10-16 18:25:16 +0200

    setup allure report

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -196,7 +196,10 @@ export const config: WebdriverIO.Config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec', ['allure', { outputDir: 'allure-results' }]],
+    //reporters: ['spec', ['allure', { outputDir: 'allure-results' }]],
+    reporters: [['allure', {
+        outputDir: 'allure-results'
+    }]],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
